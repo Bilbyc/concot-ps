a8043bc610a45a184154d19bfd9eaf8716af5699
Carlos Bilby <carlos@acception.com.br>
2023-11-07 17:52:58 -0300

    refactor: alter autenticar gsp to use grails tags

---
+++ grails-app/taglib/concot/ConcotTagLib.groovy
@@ -0,0 +1,8 @@
+package concot
+
+class ConcotTagLib {
+    static namespace = 'concot'
+
+    static defaultEncodeAs = [taglib:'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+}
