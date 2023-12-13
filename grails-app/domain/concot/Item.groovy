6c11bfc1b4f5a4be9fd7ba6f8d34b1495668ab6e
Carlos Bilby <carlos@acception.com.br>
2023-12-13 17:50:16 -0300

    feat: initiate itemService class

--- grails-app/domain/concot/Item.groovy
+++ grails-app/domain/concot/Item.groovy
@@ -3,10 +3,12 @@ package concot
 class Item {
 
     String nome
+    byte[] file
     static belongsTo = [categoria: Categoria]
 
     static constraints = {
         nome nullable:false, blank:false, maxSize:128
+        file nullable: true
         categoria nullable:false
     }
 }
---
+++ grails-app/services/concot/ItemService.groovy
@@ -0,0 +1,9 @@
+package concot
+
+import grails.transaction.Transactional
+
+@Transactional
+class ItemService {
+
+
+}
